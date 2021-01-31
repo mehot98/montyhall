f748c936f3274ad633117de332f8722e6e6b72a8
mehot98 <76448240+mehot98@users.noreply.github.com>
2021-01-31 12:44:29 +0900

    second commit

--- door-group.js
+++ door-group.js
@@ -4,27 +4,28 @@ export class DoorGroup{
     constructor(stageWidth, stageHeight, num){
         this.stageWidth = stageWidth;
         this.stageHeight = stageHeight;
-        this.num = num;
+                
+        this.height = this.stageHeight / (num * 1.25 + 0.25);
+        this.width = this.height / 1.618;
         
+        this.num_height = num;
+        this.num_width = parseInt(this.stageWidth / (this.width * 1.4));
 
-        this.width = stageWidth / (num * 2 + 1);
-        this.height = this.width * 1.618;
-
-        this.heigth_gap = (this.stageHeight - (this.height * num)) / (num + 1);
-
+        this.heigth_gap = this.height / 4;
+        this.width_gap = (this.stageWidth - (this.width * this.num_width)) / (this.num_width + 1)
     }
 
     animate(ctx){
         const door = [];
 
-        for(let i = 0; i < this.num; i++){
+        for(let i = 0; i < this.num_width; i++){
 
             door[i] = [];
 
-            for(let j = 0; j < this.num; j++){
+            for(let j = 0; j < this.num_height; j++){
 
                 door[i][j] = new Door(
-                    this.width * (2 * i + 1),
+                    this.width_gap * (i + 1) + this.width * i,
                     this.heigth_gap * (j + 1) + this.height * j,
                     this.width,
                     this.height,
